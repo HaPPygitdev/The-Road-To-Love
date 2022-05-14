d222cf39f0dbc476f49663b7fef963ea64737095
KolbasaRF <marmar.2004@yandex.ru>
2022-05-14 11:52:50 +0300

    Without API3

--- registration/forms.py
+++ registration/forms.py
@@ -25,11 +25,34 @@ class LoginForm(ModelForm):
 #     password = forms.CharField(max_length=100)
 
 
-class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    confirm_password = forms.CharField(label="Confirm password", max_length=100)
-    first_name = forms.CharField(label="First name", max_length=100)
-    second_name = forms.CharField(label="Surname", max_length=100)
-    age = forms.CharField(label="Age", max_length=3)
-    gender = forms.ChoiceField(label="Your gender: ", choices=PIS_TYPE)
+class SignUpForm(ModelForm):
+    class Meta:
+        model = Temporary
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'second_name', 'age', 'gender']
+
+        widgets = {
+            "username": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }),
+            "password": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }),
+            "confirm_password": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm Password'
+            }),
+            "first_name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'First name'
+            }),
+            "second_name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Second name'
+            }),
+            "age": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Age'
+            })
+        }
